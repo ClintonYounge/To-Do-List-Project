29f88856b9a4088276e40266559186b155ba5391
ClintonYounge <clintonyounge18@gmail.com>
2023-02-28 21:04:46 -0400

    Update to-do-list structure
    -Update list structure
    -Apply css to match specified design

--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const listTasks = [
+  {
+    description: 'Buy groceries',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Do laundry',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Clean the house',
+    completed: false,
+    index: 2,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+function renderTasks() {
+  const taskList = document.querySelector('#task-list');
 
-  return element;
+  listTasks.forEach((task, index) => {
+    const listItem = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.completed;
+
+    const taskDescription = document.createElement('p');
+    taskDescription.innerHTML = task.description;
+    taskDescription.classList.add('task-desc');
+    listItem.appendChild(checkbox);
+    listItem.appendChild(taskDescription);
+
+    const menuIcon = document.createElement('span');
+    menuIcon.classList.add('fas', 'fa-ellipsis-v', 'menuIcon');
+    listItem.appendChild(menuIcon);
+    taskList.appendChild(listItem);
+
+    if (index >= 0) {
+      const horizontalRule = document.createElement('hr');
+      horizontalRule.classList.add('list-hr');
+      taskList.appendChild(horizontalRule);
+    }
+  });
 }
 
-document.body.appendChild(component());
+renderTasks();
