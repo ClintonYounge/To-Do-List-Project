139c0543590f8229a66b1dae691ee96a507f52b4
ClintonYounge <clintonyounge18@gmail.com>
2023-03-09 16:37:25 -0400

    Update to use mock local storage

--- __test__/task.test.js
+++ __test__/task.test.js
@@ -2,30 +2,65 @@
  * @jest-environment jsdom
  */
 
-import { addTask, deleteTask, taskList } from '../src/module/task.js';
+import { addTask, deleteTask } from '../src/module/task.js';
+
+// Set up mock localStorage
+const localStorageMock = (() => {
+  let store = {};
+  return {
+    getItem: (key) => store[key] || null,
+    setItem: (key, value) => {
+      store[key] = value.toString();
+    },
+    removeItem: (key) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    },
+  };
+})();
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
 
 // Tests for addTask function
 describe('addTask function', () => {
-  test('should add a new task to the task list', () => {
-    addTask('Task 2');
-    expect(taskList.children.length).toBe(2);
-    expect(taskList.children[1].querySelector('.task-desc').textContent).toBe('Task 2');
+  beforeEach(() => {
+    localStorage.clear();
   });
+
+  test('should add a new task to localStorage', () => {
+    addTask('Task 1');
+    expect(localStorage.getItem('task-0')).toBe('Task 1');
+  });
+
+  test('should increment the task count in localStorage', () => {
+    addTask('Task 1');
+    addTask('Task 2');
+    addTask('Task 3');
+    expect(localStorage.getItem('taskCount')).toBe('3');
+  });
+
   test('should not add a task if the description is empty', () => {
     addTask('');
-    expect(taskList.children.length).toBe(2);
+    expect(localStorage.getItem('taskCount')).toBeNull();
   });
 });
+
 // Tests for deleteTask function
 describe('deleteTask function', () => {
-  test('should delete a task from the task list', () => {
-    const deleteButton = taskList.children[0].querySelector('.delete-task-button');
-    const taskItem = deleteButton.parentElement;
-    deleteTask(taskItem);
-    expect(taskList.children.length).toBe(1);
+  beforeEach(() => {
+    localStorage.clear();
+    localStorage.setItem('taskCount', '2');
+    localStorage.setItem('task-0', 'Task 1');
+    localStorage.setItem('task-1', 'Task 2');
   });
-  test('should not delete a task if the index is out of range', () => {
-    deleteTask(taskList.children.length);
-    expect(taskList.children.length).toBe(1);
+
+  test('should remove the specified task from localStorage', () => {
+    deleteTask('task-1');
+    expect(localStorage.getItem('task-1')).toBeNull();
+  });
+
+  test('should not throw an error if the specified task does not exist', () => {
+    expect(() => deleteTask('task-2')).not.toThrow();
   });
 });
--- src/module/task.js
+++ src/module/task.js
@@ -1,60 +1,14 @@
-// Mock the localStorage object
-const localStorageMock = (() => {
-  let store = {};
-  return {
-    getItem: (key) => store[key] || null,
-    setItem: (key, value) => {
-      store[key] = value.toString();
-    },
-    removeItem: (key) => {
-      delete store[key];
-    },
-    clear: () => {
-      store = {};
-    },
-  };
-})();
-Object.defineProperty(window, 'localStorage', { value: localStorageMock });
-// Mock the HTML elements
-document.body.innerHTML = `
-    <ul id="task-list">
-      <li>
-        <input type="checkbox" class="checkboxes">
-        <span class="task-desc">Task 1</span>
-        <span class="delete-task-button"><i class="fas fa-trash"></i></span>
-      </li>
-    </ul>
-  `;
-export const taskList = document.querySelector('#task-list');
-// Update the index property of each task in the array
-function updateTaskIndexes() {
-  for (let i = 0; i < taskList.length; i += 1) {
-    taskList[i].index = i;
-    const taskElement = taskList.children[i];
-    const checkbox = taskElement.querySelector('.checkboxes');
-    const deleteButton = taskElement.querySelector('.delete-task-button');
-    checkbox.dataset.index = i;
-    deleteButton.dataset.index = i;
-  }
-}
 // Add a new task to the list
 export function addTask(description) {
-  if (!description) {
-    return; // If the description is empty, don't add the task
+  const taskCount = parseInt(localStorage.getItem('taskCount'), 10) || 0;
+  const key = `task-${taskCount}`;
+  if (description.trim() !== '') {
+    localStorage.setItem(key, description.trim());
+    localStorage.setItem('taskCount', taskCount + 1);
   }
-  const newTaskElement = document.createElement('li');
-  newTaskElement.innerHTML = `
-      <input type="checkbox" class="checkboxes">
-      <span class="task-desc">${description}</span>
-      <span class="delete-task-button"><i class="fas fa-trash"></i></span>
-    `;
-  taskList.appendChild(newTaskElement);
-  updateTaskIndexes();
 }
+
 // Delete a task from the list
-export function deleteTask(taskItem) {
-  if (taskItem && taskItem.remove) {
-    taskItem.remove();
-    updateTaskIndexes();
-  }
+export function deleteTask(taskId) {
+  localStorage.removeItem(taskId);
 }
